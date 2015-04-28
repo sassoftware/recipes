4c36ddf731a1fc521a66d1aa06ebcf2cb7357a9f
Brett Smith <bc.smith@sas.com>
2015-04-28 08:28:46 -0400

    Update open-vm-tools to 9.4.6 APPENG-3499

--- platforms/open-vm-tools/redirect.sh
+++ platforms/open-vm-tools/redirect.sh
@@ -4,3 +4,4 @@ sed -i 's|^hardcode_libdir_flag_spec=.*|hardcode_libdir_flag_spec=""|g' libtool
 
 sed -i 's|^runpath_var=LD_RUN_PATH|runpath_var=NO_RPATH_NO|g' libtool
 
+sed -i -e 's! -shared ! -Wl,--as-needed\0!g' libtool
